ce1f8522517e183221a7456bb77dce07d87e2870
Taichi Ishitani <taichi730@gmail.com>
2022-07-05 22:29:49 +0900

    support row0/1trg bit field types
    (refs: rggen/rggen#109)

--- rggen_bit_field_w01trg.v
+++ rggen_bit_field_w01trg.v
@@ -4,29 +4,31 @@ module rggen_bit_field_w01trg #(
 )(
   input               i_clk,
   input               i_rst_n,
-  input               i_bit_field_valid,
-  input   [WIDTH-1:0] i_bit_field_read_mask,
-  input   [WIDTH-1:0] i_bit_field_write_mask,
-  input   [WIDTH-1:0] i_bit_field_write_data,
-  output  [WIDTH-1:0] o_bit_field_read_data,
-  output  [WIDTH-1:0] o_bit_field_value,
+  input               i_sw_valid,
+  input   [WIDTH-1:0] i_sw_read_mask,
+  input               i_sw_write_enable,
+  input   [WIDTH-1:0] i_sw_write_mask,
+  input   [WIDTH-1:0] i_sw_write_data,
+  output  [WIDTH-1:0] o_sw_read_data,
+  output  [WIDTH-1:0] o_sw_value,
+  input   [WIDTH-1:0] i_value,
   output  [WIDTH-1:0] o_trigger
 );
   reg [WIDTH-1:0] r_trigger;
 
-  assign  o_bit_field_read_data = {WIDTH{1'b0}};
-  assign  o_bit_field_value     = r_trigger;
-  assign  o_trigger             = r_trigger;
+  assign  o_sw_read_data  = i_value;
+  assign  o_sw_value      = r_trigger;
+  assign  o_trigger       = r_trigger;
 
   always @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
       r_trigger <= {WIDTH{1'b0}};
     end
-    else if (i_bit_field_valid) begin
+    else if (i_sw_valid) begin
       r_trigger <=
         (TRIGGER_VALUE != 0)
-          ? i_bit_field_write_mask & ( i_bit_field_write_data)
-          : i_bit_field_write_mask & (~i_bit_field_write_data);
+          ? i_sw_write_mask & ( i_sw_write_data)
+          : i_sw_write_mask & (~i_sw_write_data);
     end
     else if (r_trigger != {WIDTH{1'b0}}) begin
       r_trigger <= {WIDTH{1'b0}};
